84ee581f5c67632553d5691e48251d1eb22b043b
Rajkumar-Marimithu <rajkumartechgy@gmail.com>
2023-11-20 17:46:23 +0000

    project init

---
+++ src/test/java/com/rojatech/lms/sivagami/SivagamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojatech.lms.sivagami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SivagamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
